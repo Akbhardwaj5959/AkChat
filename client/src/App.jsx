f02246aedd7749371a9d11d6064c873586ef1c37
Akbhardwaj5959 <riyajay31@gmail.com>
2025-02-03 20:36:53 +0530

    chat application completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,62 +1,102 @@
-import { Button, Container, TextField, Typography } from '@mui/material';
-import { useEffect, useMemo, useState } from 'react';
-import React from 'react';
-import {io} from 'socket.io-client';
-// ye socket name of client k liyye h 
-const App= ()=> {
-
-  const socket = useMemo(()=>io('http://localhost:3000/'), []);
+import { Button, Container, Stack, TextField, Typography } from "@mui/material";
+import { useEffect, useMemo, useState } from "react";
+import React from "react";
+import { io } from "socket.io-client";
+// ye socket name of client k liyye h
+const App = () => {
+  const socket = useMemo(() => io("http://localhost:3000/"), []);
 
   const [sokectId, setsocketId] = useState("");
   const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [room, setRoom] = useState(""); //ye ek spesific room m msg bhejne k liye h
 
-  const handleSubmit= (e)=>{
-    e.preventDefault();
-    console.log(message);
-    //in order to send  a message to use emit
-    socket.emit("message", message);
-    console.log("message sent")
-    setMessage("");
+
+  const joinRoom = (e)=>{
+    e.preventDefault()
+    console.log({room}, "joined");
+    socket.emit("join-room", {room});
+    setRoom("");
+
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ message, room });
+    //in order to send  a message to use emit
+    socket.emit("message", { message, room });
+    console.log("message sent");
+    setMessage("");
+  };
 
-// server exectly kaha per live h 
-  useEffect(()=>{
-    socket.on("connect", ()=>{
+  // server exectly kaha per live h
+  useEffect(() => {
+    socket.on("connect", () => {
       setsocketId(socket.id);
       console.log(`connected  ${socket.id}`);
-    })
+    });
 
-    socket.on("forward-message", (data)=>{
-      console.log(data.message);
-   });
+    socket.on("forward-message", (data) => {
+      // console.log(data.message);
+      setMessages((messages) => [...messages, data.message]);
+    });
 
-  }, [])
+    return ()=>{
+      socket.disconnect();
+    }
+  }, []);
   return (
     <>
-     <Container>
+      <Container>
 
-      <Typography variant = "h3" component="h2">
-        Welcome to the AkChat Client App
-      </Typography>
-      <Typography variant = "h4" component="h5">
-        socket Id {socket.id}
-      </Typography>
-
-      <form onSubmit={handleSubmit}>
-        <TextField onChange={(e)=>setMessage(e.target.value)} id="outline-basic" label="Type a message" variant = "outlined" />
-        <Button type="submit" variant="contained" color="primary" >
-          Send
-        </Button>
-      </form>
+        
 
 
+        <Typography variant="h3" component="h2">
+          Welcome to the AkChat Client App
+        </Typography>
+        <Typography variant="h4" component="h5">
+          socket Id {socket.id}
+        </Typography>
 
-     </Container>
-     
-    
+        <form onSubmit={joinRoom}>
+          <TextField onChange={(e)=>setRoom(e.target.value)}
+            label= "Room Name"
+            variant="outlined"
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Join Room
+            </Button>
+        </form>
+
+        <form onSubmit={handleSubmit}>
+          <TextField
+            onChange={(e) => setMessage(e.target.value)}
+            id="outline-basic"
+            label="Type a message"
+            variant="outlined"
+          />
+          <TextField
+            onChange={(e) => setRoom(e.target.value)}
+            type="text"
+            label="Room"
+            variant="outlined"
+          />
+          <Button type="submit" variant="contained" color="primary">
+            Send
+          </Button>
+        </form>
+
+        <Stack>
+          {messages.map((message, index) => (
+            <Typography key={index} variant="h6" component="div" gutterBottom>
+              {message}
+            </Typography>
+          ))}
+        </Stack>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,58 +1,63 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import {createServer} from 'http';
+import { createServer } from "http";
 //implement socket.io on top of http
 const server = createServer(app);
 // secure server using handshake
-import {Server} from 'socket.io';
+import { Server } from "socket.io";
 // socket.io's server instance is used for creating  a new connection
-import cors from 'cors';
+import cors from "cors";
 
 // socket server on top of http server
 //along with implemetation of cors for server protaction
-const  io = new Server(server, {
-    cors:{
-        origin:'*',  //allow all origins
-        methods: ["GET", "POST"]
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "*", //allow all origins
+    methods: ["GET", "POST"],
+  },
 });
 
-// socket.io middlewear  for implement the handshake lisinging part client ka 
-io.use((socket, next)=>{
-    next();
+// socket.io middlewear  for implement the handshake lisinging part client ka
+io.use((socket, next) => {
+  next();
 });
-
 
 // socket.io connection event client ko sunna h yaha se
-io.on('connection', (socket)=>{
-     console.log(`user has been connecting ${socket.id}`);
+io.on("connection", (socket) => {
+  console.log(`user has been connecting ${socket.id}`);
 
-     // ye message humare server per aagya h 
-     socket.on("message", (data)=>{
-        console.log(data);
-        socket.broadcast.emit("forward-message", {message:data});
-     });
-     
+  // ye message humare server per aagya h
+  socket.on("message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("forward-message", { message: data.message });
+  });
+
+  socket.on("join-room", (RoomName)=>{
+    socket.join(RoomName.room);
+    console.log(`joined Room ${RoomName.room}`)
+  });
+
+  socket.on("disconnect", ()=>{
+    console.log(`a user has been disconnect ${socket.id}`);
+  });
 });
 
 // app.use(cors());
 
 //dot env
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-app.get("/", (req,res)=>{
-    res.status(200).send("Wlcome to the AkChat");
+app.get("/", (req, res) => {
+  res.status(200).send("Wlcome to the AkChat");
 });
 
 // const port = process.env.PORT || 5000;
 
-server.listen(process.env.PORT, ()=>{
-
-    try{
+server.listen(process.env.PORT, () => {
+  try {
     console.log(`server is running on that this port ${process.env.port}`);
-    }
-    catch(err){
-        console.log(err);
-    }
+  } catch (err) {
+    console.log(err);
+  }
 });
